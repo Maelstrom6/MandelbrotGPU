766941ba9e95e1c8cb44b1ceb64515ce3008e084
Chris <Chris@DESKTOP-7AL8R7F>
2019-07-13 22:32:57 +0200

    Added basic orbit traps.

--- Mandelbrot3/src/mandelbrot3/Mand.java
+++ Mandelbrot3/src/mandelbrot3/Mand.java
@@ -107,6 +107,117 @@ public class Mand {
         }
     }
     
+    public BufferedImage mapOrbit(){
+        map = new BufferedImage(sizeX, sizeY, BufferedImage.TYPE_INT_RGB);
+        iter = 0;
+        double trapx=0.5;
+        double trapy=00;
+        Thread[] column = new Thread[threads];
+        for (int i = 0; i < threads; i++) {
+            column[i] = new Thread(new Runnable() {
+                public void run() {
+                    int id = iter;
+                    iter++;
+                    //int num;
+                    int scaler, scaleg, scaleb;
+                    for (int x = id; x < sizeX; x += threads) {
+                        for (int i = 0; i < sizeY; i++) {
+                            double startingxCoordinate = (-limX * (1.0 * sizeX / sizeY) + (2.0 * limX * x / sizeY) + centreX);
+                            double startingyCoordinate = (limY - (2.0 * limY * i / sizeY) + centreY);
+                            Myi coordinate = (Transform(new Myi(startingxCoordinate, startingyCoordinate, false)));
+                            //double xCoordinate = TransformX(startingxCoordinate, startingyCoordinate);
+                            //double yCoordinate = TransformY(startingxCoordinate, startingyCoordinate);
+                            double xCoordinate = coordinate.getRe();
+                            double yCoordinate = coordinate.getIm();
+                            double dist = PartOfOrbitRing(xCoordinate, yCoordinate);
+                            //num = (int) (PartSet((-limX + (2.0 * limX * x / sizeX) + centreX), (limY - (2.0 * limY * i / sizeY)+centreY)));//-(2-xLim) for x to move it left
+                            if (dist == 0) {//need to change this if yuou change i<100
+                                map.setRGB(x, i, Color.BLACK.getRGB());
+                            } else {
+                                //double dist = Math.pow(fn.getRe()-f0.getRe(), 2)+Math.pow(fn.getIm()-f0.getIm(), 2);
+                                scaler = getRedFromN(dist);//divide everything by 4 for smoother transition
+                                scaleg = getGreenFromN(dist);
+                                scaleb = getBlueFromN(dist);
+                                map.setRGB(x, i, new Color(scaler, scaleg, scaleb).hashCode());
+                            }
+                        }
+                    }
+                }
+            });
+            column[i].setPriority(Thread.MIN_PRIORITY);
+            column[i].start();
+        }
+        for (int i = 0; i < threads; i++) {
+            try {
+                column[i].join();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Mand.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return map;
+    }
+    
+    public double PartOfOrbitDot(double x, double y){
+        Myi trapPoint = new Myi(-0.1, 0, false);
+        Myi c = new Myi(x, y, false);
+        Myi znMinOne = new Myi(x, y, false);
+        Myi zn;
+        double distance = 1000000;
+        int i = 0;
+        while (i < maxI && znMinOne.getR() < 2) {
+            zn = znMinOne.power(numer, denom).add(c);
+            i++;
+            double zMinusPointModulus = zn.add(trapPoint.ainverse()).getR();
+            if(zMinusPointModulus < distance)
+                distance = zMinusPointModulus;
+            //distance = Math.min(distance, Math.pow(zn.getRe()-znMinOne.getRe(), 2)+Math.pow(zn.getIm()-znMinOne.getIm(), 2));
+            znMinOne = zn;
+        }
+        return (distance*100);
+    }
+    
+    public double PartOfOrbitRing(double x, double y){
+        double minAcceptable=0.4, maxAcceptable=0.5;
+        Myi trapPoint = new Myi(-0.1, 0, false);
+        Myi c = new Myi(x, y, false);
+        Myi znMinOne = new Myi(x, y, false);
+        Myi zn;
+        double distance = 1000000;
+        int i = 0;
+        while (i < maxI && znMinOne.getR() < 2) {
+            zn = znMinOne.power(numer, denom).add(c);
+            i++;
+            double zMinusPointModulus = zn.add(trapPoint.ainverse()).getR();
+            if(zMinusPointModulus < distance && minAcceptable<zMinusPointModulus && zMinusPointModulus<maxAcceptable)
+                distance = zMinusPointModulus;
+            //distance = Math.min(distance, Math.pow(zn.getRe()-znMinOne.getRe(), 2)+Math.pow(zn.getIm()-znMinOne.getIm(), 2));
+            znMinOne = zn;
+        }
+        return (distance*100);
+    }
+    
+    public double PartOfOrbitCross(double x, double y){
+        Myi trapPoint = new Myi(-0.1, 0, false);
+        Myi c = new Myi(x, y, false);
+        Myi zn = new Myi(x, y, false);
+        double distance = 1000000;
+        int i = 0;
+        while (i < maxI && zn.getR() < 2) {
+            zn = zn.power(numer, denom).add(c);
+            i++;
+            double horiDist = Math.abs(trapPoint.getRe() - zn.getRe());
+            double vertDist = Math.abs(trapPoint.getIm() - zn.getIm());
+            if(vertDist < distance){
+                distance = vertDist;
+            }
+            if(horiDist < distance){
+                distance = horiDist;
+            }
+            //distance = Math.min(distance, Math.pow(zn.getRe()-znMinOne.getRe(), 2)+Math.pow(zn.getIm()-znMinOne.getIm(), 2));
+        }
+        return (distance*100);
+    }
+    
     public BufferedImage mapNoMirror() {
         map = new BufferedImage(sizeX, sizeY, BufferedImage.TYPE_INT_RGB);
         iter = 0;
--- Mandelbrot3/src/mandelbrot3/Test.java
+++ Mandelbrot3/src/mandelbrot3/Test.java
@@ -1,12 +1,6 @@
 package mandelbrot3;
 
-import java.beans.Expression;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 
 /**
  *
@@ -17,30 +11,55 @@ public class Test {
     private static int maxI = 20;
 
     public static void main(String[] args) {
-        Myi z = new Myi(1, 0, false);
-        // create a script engine manager
-        ScriptEngineManager factory = new ScriptEngineManager();
-        // create a JavaScript engine
-        ScriptEngine engine = factory.getEngineByName("JavaScript");
-        // evaluate JavaScript code from String
-        Object obj = null;
-        try {
-            obj = engine.eval("new Scanner(new File(\"hi.txt\"))");
-        } catch (ScriptException ex) {
-            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println(obj);
+        Myi z = new Myi(-0.1169, 0.75825, false);
+        System.out.println(fn(1002, z));
+        System.out.println(fMinus(z));
+        System.out.println(fPlus(z));
+        //-0.9548, 0.01
+        //Polar: (0.04858894694029913, -2.9122675954400097)	Rectangular: (-0.04731689048570509, -0.011045254163259237)
+        //Polar: (0.9527471358095793, 3.1299993344655745)	Rectangular: (-0.9526831095142948, 0.011045254163259407)
+        
+        //-0.1169, 0.75825
+        //Polar: (0.8918343008704657, 2.433638860196384)	Rectangular: (-0.6775212333728137, 0.5799425821045507)
+        //Polar: (0.028200065476293872, -1.3635858635912836)	Rectangular: (0.005801623132925245, -0.027596827025779036)
+        ArrayList<Integer> operators = new ArrayList();
+        Mand obj = new Mand(2, 1, 500, 500, 2, 2, 0, 0, 500, operators);
+        obj.savePNG(obj.mapOrbit(), System.getProperty("user.dir") + "/MyNewTest1.png");
     }
 
-    public static Myi PartOfMand(double a, double y) {
-        Myi tot = new Myi(a, y, false);
-        int i = 0;
-        while (i < maxI) {
-            tot = tot.power(2);
-            tot = tot.add(a, y);
-            i++;
+    public static Myi fMinus(Myi z){
+        //(1 + sqrt(1 - 4*z)) / 2
+        Myi result = z.multiply(4);
+        result = result.ainverse();
+        result = result.add(1,0);
+        result = result.power(1,2);
+        result = result.ainverse(); // + -
+        result = result.add(1,0);
+        result = result.multiply(0.5);
+        return result;
+    }
+    
+    public static Myi fPlus(Myi z){
+        //(1 + sqrt(1 - 4*z)) / 2
+        Myi result = z.multiply(4);
+        result = result.ainverse();
+        result = result.add(1,0);
+        result = result.power(1,2);
+        result = result.add(1,0);
+        result = result.multiply(0.5);
+        return result;
+    }
+    
+    public static Myi fn(int n, Myi z){
+        Myi result = z;
+        for(int i=0;i<n;i++){
+            result = result.power(2).add(z);
+            if(result.getR()>2){
+                result = new Myi(0,0,true);
+                break;
+            }
         }
-        return tot;
+        return result;
     }
 
 }
